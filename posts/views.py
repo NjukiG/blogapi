5431cdf65317c952e741c7ceb5369ec54ac745c7
George <gnjuki19@gmail.com>
2023-03-30 11:38:13 +0300

    Auser is able to create a post and perform CRUD, however, only the author of the post is able to update and delete their own post. Admin is able to do all CRUD functions

--- posts/views.py
+++ posts/views.py
@@ -3,15 +3,21 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly  # new
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
 
 # Create your views here.
+
+# permission_classes = (permissions.IsAdminUser,) # new
+
