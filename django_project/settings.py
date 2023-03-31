9eec4c1408885d8380f792165ed9729581e04302
George <gnjuki19@gmail.com>
2023-03-31 12:32:44 +0300

    Added a schema and documentation using spectacular package

--- django_project/settings.py
+++ django_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount", # new
     "dj_rest_auth", # new
     "dj_rest_auth.registration", # new
+    "drf_spectacular", # new
 
     # Local
     "accounts.apps.AccountsConfig", # new
@@ -63,6 +64,7 @@ REST_FRAMEWORK = { # new
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication", # new
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema", # new
 }
 
 
@@ -170,4 +172,11 @@ CORS_ORIGIN_WHITELIST = (
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]  # new
 
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to learn about DRF",
+    "VERSION": "1.0.0",
+    # OTHER SETTINGS
+}
 
