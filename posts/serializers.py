655c121d223f22285b94434cfd327b3cf20bd77e
George <gnjuki19@gmail.com>
2023-03-31 11:40:20 +0300

    Added serializers for users and routes for them too

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model  # new
 from rest_framework import serializers
 
 from .models import Post
@@ -13,4 +14,9 @@ class PostSerializer(serializers.ModelSerializer):
         )
         model = Post
 
-        
+
+class UserSerializer(serializers.ModelSerializer): # new
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username",)
+            
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+from django.contrib.auth import get_user_model  # new
 from rest_framework import generics
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly  # new
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer  # new
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,) # new
@@ -15,7 +16,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
+
+class UserList(generics.ListCreateAPIView): # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):  # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
 
 # Create your views here.
 
